a0aea5d6650d7081e0c07ebd077fb19d6375ce5b
unknown <x23186615@student.ncirl.ie>
2024-04-19 20:05:05 +0100

    first commit

---
+++ Coffee/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Coffee
+
+
+class CoffeeAdmin(admin.ModelAdmin):
+    list_display = ('name','price','quantity')
+
+admin.site.register(Coffee, CoffeeAdmin)
---
+++ Coffee/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+
+
+class Coffee(models.Model):
+    name = models.CharField( max_length=255)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    image = models.CharField(max_length=2083)
